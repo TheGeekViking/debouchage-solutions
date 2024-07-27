73a0d441c5d9128f2222395f7ef8fc799e645847
The Geek Viking <c.sergerie@hotmail.fr>
2024-07-26 23:24:40 -0400

    Navbar finished

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,23 @@
 module.exports = {
   content: ["./*.html"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        blue: {
+          50: "#f0f9ff",
+          100: "#e0f3fe",
+          200: "#b9e8fe",
+          300: "#7cd6fd",
+          400: "#36c3fa",
+          500: "#0caceb",
+          600: "#0092d5",
+          700: "#016da3",
+          800: "#065c86",
+          900: "#0b4d6f",
+          950: "#07314a",
+        },
+      },
+    },
   },
   plugins: [],
 };

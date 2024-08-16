978ed4bcfdd75d36b28cc4ef425fcad0e2a6020c
The Geek Viking <c.sergerie@hotmail.fr>
2024-08-16 12:00:00 -0400

    Mods on hero section

--- js/script.js
+++ js/script.js
@@ -2,6 +2,7 @@
 const btnHamburger = document.getElementById("btn-hamburger");
 const mobileMenu = document.getElementById("mobile-menu");
 const btnNavbarClose = document.getElementById("navbar-close");
+const navItems = document.querySelectorAll("#mobile-menu a"); // Select all nav items
 
 btnNavbarClose.addEventListener("click", () => {
   mobileMenu.classList.toggle("hidden");
@@ -9,6 +10,13 @@ btnNavbarClose.addEventListener("click", () => {
 
 btnHamburger.addEventListener("click", () => {
   mobileMenu.classList.toggle("hidden");
+});
+
+// Close mobile menu when a nav item is clicked
+navItems.forEach((item) => {
+  item.addEventListener("click", () => {
+    mobileMenu.classList.add("hidden"); // Ensure the menu closes
+  });
 });
 
 ///////////// FAQ
